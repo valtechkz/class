f0d7be9bc844b50c8cfc98b1dac2d0832680b85e
kiril <kiril.zafirov@valtech.com>
2024-12-12 17:57:24 +0100

    Auto-generated commit

--- src/app/models/activity.ts
+++
@@ -1,8 +0,0 @@
-export interface Activity {
-  dateId: number[];
-  description: string;
-  documentUrl: string | null;
-  filterCombinationId: number;
-  name: string;
-  videoUrl: string | null;
-}
